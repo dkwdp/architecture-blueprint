069216b851afb61e01203964d9426f8e3a71a695
Bluemi <bruno.schilling@protonmail.ch>
2025-10-22 13:24:59 +0200

    add Level types

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -2,6 +2,49 @@ import { z } from 'zod';
 
 export const LevelType = z.enum(['video','text','quiz','interactive','switch']);
 
+export const LevelId = z.object({
+	mapId: z.string(),
+	levelId: z.string(),
+	serverUrl: z.url().optional(),
+});
+
+export const LevelBase = z.object({
+	id: z.string(),
+	title: z.string(),
+	type: LevelType,
+	next_level: LevelId.optional(),
+});
+
+export const LevelContent = z.discriminatedUnion('type', [
+	LevelBase.extend({
+		type: z.literal('video'),
+		video_url: z.url(),
+		start_button_url: z.url(),
+	}),
+	LevelBase.extend({
+		type: z.literal('text'),
+		markdown: z.string()
+	}),
+	/*
+	LevelBase.extend({
+		type: z.literal('quiz'),
+		questions: z.array(z.object({
+			q: z.string(),
+			choices: z.array(z.string()).min(2),
+			answer: z.number().int()
+		}))
+	}),
+	 */
+	LevelBase.extend({
+		type: z.literal('interactive'),
+		entrypoint: z.string(),
+	}),
+	LevelBase.extend({
+		type: z.literal('switch'),
+		next_level: z.array(LevelId),
+	})
+]);
+
 export const MapStructure = z.object({
   id: z.string(),
   title: z.string(),
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,10 +1,66 @@
 export type LevelType = 'video'|'text'|'quiz'|'interactive'|'switch';
 
+interface LevelIdImpl {
+	mapId: string,
+	levelId: string,
+	serverUrl: string | null,
+}
+export type LevelId = LevelIdImpl | null;
+
+export interface LevelBase {
+	id: string,
+	title: string,
+	type: LevelType,
+}
+
+export interface LevelVideo extends LevelBase {
+	type: 'video';
+	video_url: string;
+	start_button_url: string;
+	next_level: LevelId,
+}
+
+export interface LevelText extends LevelBase {
+	type: 'text';
+	markdown: string;
+	next_level: LevelId,
+}
+
+/*
+export interface LevelQuiz extends LevelBase {
+  type: 'quiz';
+  questions: {
+    q: string;
+    choices: string[];
+    answer: number;
+  }[];
+	next_level: LevelId,
+}
+ */
+
+export interface LevelInteractive extends LevelBase {
+	type: 'interactive';
+	entrypoint: string;
+	next_level: LevelId,
+}
+
+export interface LevelSwitch extends LevelBase {
+	type: 'switch';
+	next_level: (LevelId)[];
+}
+
+export type LevelContent =
+	| LevelVideo
+	| LevelText
+	// | LevelQuiz
+	| LevelInteractive
+	| LevelSwitch;
+
 export interface MapNode {
   id: string;
   title: string;
   type: LevelType;
-  pos: [number,number];
+  pos: [number, number];
 }
 
 export interface MapStructure {
