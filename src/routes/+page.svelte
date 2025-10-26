9227623b5742d25708d885a0409a55351b6f26d0
Bluemi <bruno.schilling@protonmail.ch>
2025-10-26 08:54:46 +0100

    update name in title page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { resolve } from '$app/paths';
 </script>
 
-<h1 class="text-3xl font-bold">LearnMaps</h1>
+<h1 class="text-3xl font-bold">Der kleine Weg des Programmierens - Title Page</h1>
 <ul class="mt-4 list-disc pl-6">
 	<li>
 		<a class="underline" href="{resolve('/map/map01')}">Start</a>
