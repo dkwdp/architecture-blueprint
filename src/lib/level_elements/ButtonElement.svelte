b01c8fc30e81bfa84c4e301211a42493bc144de5
jonathan-d-z <jonathan.d.zieger@gmail.com>
2025-11-16 22:34:48 +0100

    first styles for ButtonElement and NextLevelButton

--- src/lib/level_elements/ButtonElement.svelte
+++ src/lib/level_elements/ButtonElement.svelte
@@ -13,4 +13,34 @@
 	}
 </script>
 
+<style>
+	button {
+		color: #f6f4f1;
+		cursor: pointer;
+		background-color: #62DB6E;
+		height: 80px;
+		width: 200px;
+		font-size: 40px;
+		border-radius: 20px;
+		border: 2px solid #306B36;
+		font-family: "Changa One", sans-serif;
+		box-shadow: 0px 6px #002b63;
+
+		text-shadow:
+            -1px -1px 0 #306B36,
+             1px -1px 0 #306B36,
+            -1px  1px 0 #306B36,
+             1px  1px 0 #306B36;
+
+		transition:
+            transform 0.1s ease-out,
+            box-shadow 0.1s ease-out;
+        
+	}
+	button:hover{
+		 transform: translateY(3px);
+        box-shadow: 0px 3px #002b63;
+	}
+</style>
+
 <button onclick="{openOrFocus}">{element.text}</button>
--- src/lib/level_elements/NextLevelButton.svelte
+++ src/lib/level_elements/NextLevelButton.svelte
@@ -11,4 +11,29 @@
 
 </script>
 
-<button onclick="{openLevelId}">nextLevel</button>
+<style>
+	button {
+		color: #004C61;
+		cursor: pointer;
+		background-color: #43AAC4;
+		height: 28rem;
+		width: 6rem;
+		font-size: 5rem;
+		border-radius: 8px;
+		border: 2px solid #004C61;;
+		font-family: "Changa One", sans-serif;
+		box-shadow: 0px 6px #002b63;
+		justify-content: center;
+
+		transition:
+            transform 0.1s ease-out,
+            box-shadow 0.1s ease-out;
+        
+	}
+	button:hover{
+		 transform: translateY(3px);
+        box-shadow: 0px 3px #002b63;
+	}
+</style>
+
+<button onclick="{openLevelId}" class="mdi mdi-arrow-right-thick"></button>
