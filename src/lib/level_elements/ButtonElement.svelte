43f01d2128723f1cdc47565ef533def72ffb1103
jonathan-d-z <jonathan.d.zieger@gmail.com>
2025-12-12 18:57:24 +0100

    button_styles formatted for pull request

--- src/lib/level_elements/ButtonElement.svelte
+++ src/lib/level_elements/ButtonElement.svelte
@@ -13,34 +13,33 @@
 	}
 </script>
 
+<button onclick={openOrFocus}>{element.text}</button>
+
 <style>
 	button {
 		color: #f6f4f1;
 		cursor: pointer;
-		background-color: #29BE1A;
+		background-color: #29be1a;
 		height: 5rem;
 		width: 13rem;
 		font-size: 3rem;
 		border-radius: 2rem;
-		border: 2px solid #306B36;
-		font-family: "Changa One", sans-serif;
-		box-shadow: 0px 6px #0B3307;
+		border: 2px solid #306b36;
+		font-family: 'Changa One', sans-serif;
+		box-shadow: 0px 6px #0b3307;
 
 		text-shadow:
-            -1px -1px 0 #306B36,
-             1px -1px 0 #306B36,
-            -1px  1px 0 #306B36,
-             1px  1px 0 #306B36;
+			-1px -1px 0 #306b36,
+			1px -1px 0 #306b36,
+			-1px 1px 0 #306b36,
+			1px 1px 0 #306b36;
 
 		transition:
-            transform 0.1s ease-out,
-            box-shadow 0.1s ease-out;
-        
+			transform 0.1s ease-out,
+			box-shadow 0.1s ease-out;
 	}
-	button:hover{
-		 transform: translateY(3px);
-        box-shadow: 0px 3px #0B3307;
+	button:hover {
+		transform: translateY(3px);
+		box-shadow: 0px 3px #0b3307;
 	}
 </style>
-
-<button onclick="{openOrFocus}">{element.text}</button>
--- src/lib/level_elements/NextLevelButton.svelte
+++ src/lib/level_elements/NextLevelButton.svelte
@@ -8,32 +8,30 @@
 		if (page.params.mapId == null) throw new Error('mapId is null');
 		normalize_and_goto(levelId, page.params.mapId);
 	}
-
 </script>
+
+<button onclick={openLevelId} class="mdi mdi-arrow-right-thick"></button>
 
 <style>
 	button {
 		color: #094579;
 		cursor: pointer;
-		background-color: #2180B6;
+		background-color: #2180b6;
 		height: 28rem;
 		width: 6rem;
 		font-size: 5rem;
 		border-radius: 1rem;
-		border: 2px solid #004C61;
-		font-family: "Changa One", sans-serif;
+		border: 2px solid #004c61;
+		font-family: 'Changa One', sans-serif;
 		box-shadow: 0px 6px #092230;
 		justify-content: center;
 
 		transition:
-            transform 0.1s ease-out,
-            box-shadow 0.1s ease-out;
-        
+			transform 0.1s ease-out,
+			box-shadow 0.1s ease-out;
 	}
-	button:hover{
-		 transform: translateY(3px);
-        box-shadow: 0px 3px #092230;
+	button:hover {
+		transform: translateY(3px);
+		box-shadow: 0px 3px #092230;
 	}
 </style>
-
-<button onclick="{openLevelId}" class="mdi mdi-arrow-right-thick"></button>
